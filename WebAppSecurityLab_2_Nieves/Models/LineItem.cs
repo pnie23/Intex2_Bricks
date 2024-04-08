04be09a8c4a5daef33ccc054a88f476c1ccf3618
pnie23 <150399640+pnie23@users.noreply.github.com>
2024-04-08 16:07:19 -0600

    Fixed all .cs models

--- WebAppSecurityLab_2_Nieves/Models/LineItem.cs
+++ WebAppSecurityLab_2_Nieves/Models/LineItem.cs
@@ -1,6 +1,15 @@
-﻿namespace Intex2_Bricks.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Intex2_Bricks.Models
 {
     public class LineItem
     {
+        [Key]
+        public int transaction_Id { get; set; }
+        [Key]
+        public int product_Id { get; set; }
+        public int qty { get; set; }
+        public int? rating { get; set; }
     }
 }
--- WebAppSecurityLab_2_Nieves/Models/Order.cs
+++ WebAppSecurityLab_2_Nieves/Models/Order.cs
@@ -1,6 +1,23 @@
-﻿namespace Intex2_Bricks.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Intex2_Bricks.Models
 {
     public class Order
     {
+        [Key]
+        public int transaction_Id { get; set; }
+        public int customer_Id  { get; set; }
+        public DateOnly date { get; set; }
+        public string day_of_week { get; set; }
+        public int time { get; set; }
+        public string entry_made { get; set; }
+        public int amount { get; set; }
+        public string type_of_transaction { get; set; }
+        public string country_of_transaction { get; set; }
+        public string shipping_address { get; set; }
+        public string bank { get; set; }
+        public string type_of_card { get; set; }
+        public bool fraud { get; set; }
+        public bool fulfilled { get; set; }
     }
 }
