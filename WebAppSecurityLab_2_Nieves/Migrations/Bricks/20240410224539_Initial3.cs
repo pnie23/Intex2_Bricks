a8209bda7b504e1524093be81fc88e6a6b6d7a92
pnie23 <150399640+pnie23@users.noreply.github.com>
2024-04-10 16:54:07 -0600

    Working checkout

---
+++ WebAppSecurityLab_2_Nieves/Migrations/Bricks/20240410224539_Initial3.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intex2_Bricks.Migrations.Bricks
+{
+    /// <inheritdoc />
+    public partial class Initial3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "shipping_address",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "CartLine",
+                columns: table => new
+                {
+                    CartLineId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    product_Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ordertransaction_Id = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartLine", x => x.CartLineId);
+                    table.ForeignKey(
+                        name: "FK_CartLine_Orders_Ordertransaction_Id",
+                        column: x => x.Ordertransaction_Id,
+                        principalTable: "Orders",
+                        principalColumn: "transaction_Id");
+                    table.ForeignKey(
+                        name: "FK_CartLine_Products_product_Id",
+                        column: x => x.product_Id,
+                        principalTable: "Products",
+                        principalColumn: "product_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartLine_Ordertransaction_Id",
+                table: "CartLine",
+                column: "Ordertransaction_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartLine_product_Id",
+                table: "CartLine",
+                column: "product_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartLine");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "shipping_address",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
