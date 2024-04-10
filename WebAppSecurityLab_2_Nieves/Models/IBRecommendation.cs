555ed36338c21e7642722d09e254c64db8d43e7b
pnie23 <150399640+pnie23@users.noreply.github.com>
2024-04-10 17:48:12 -0600

    Help

--- WebAppSecurityLab_2_Nieves/Models/IBRecommendation.cs
+++ WebAppSecurityLab_2_Nieves/Models/IBRecommendation.cs
@@ -8,8 +8,20 @@ namespace Intex2_Bricks.Models
         [Required]
         public string original_product { get; set; }
         public int recommended_product_1 { get; set; }
+        public int recommended_product_1_Name { get; set; }
+        public int recommended_product_1_Year { get; set; }
+        public int recommended_product_1_Img_link { get; set; }
         public int recommended_product_2 { get; set; }
+        public int recommended_product_2_Name { get; set; }
+        public int recommended_product_2_Year { get; set; }
+        public int recommended_product_2_Img_link { get; set; }
         public int recommended_product_3 { get; set; }
+        public int recommended_product_3_Name { get; set; }
+        public int recommended_product_3_Year { get; set; }
+        public int recommended_product_3_Img_link { get; set; }
         public int recommended_product_4 { get; set; }
+        public int recommended_product_4_Name { get; set; }
+        public int recommended_product_4_Year { get; set; }
+        public int recommended_product_4_Img_link { get; set; }
     }
 }
